8583dea5221fb21cc120fe0351e55f7ab2896803
Siva Kumar Kolli <sivakolli7482@gmail.com>
2024-08-29 07:25:59 +0530

    Add project files.

---
+++ mvcproject2/Areas/Identity/Data/sampleDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using mvcproject2.Areas.Identity.Data;
+
+namespace mvcproject2.Areas.Identity.Data;
+
+public class sampleDbContext : IdentityDbContext<sampleUser>
+{
+    public sampleDbContext(DbContextOptions<sampleDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+        builder.ApplyConfiguration(new ApplicationUserEntityConfiguration());
+    }
+}
+public class ApplicationUserEntityConfiguration : IEntityTypeConfiguration<sampleUser>
+{
+    public void Configure(EntityTypeBuilder<sampleUser> builder)
+    {
+        builder.Property(x => x.name).HasMaxLength(30);
+    }
+}
